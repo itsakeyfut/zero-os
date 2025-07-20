18702d873af464ce25a6509900e98c23608e7dce
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-07-20 21:19:14 +0900

    feat: marco - debug_print

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,6 +7,19 @@
 #![no_main]
 #![feature(panic_info_message)]
 
-fn main() {
-    println!("Hello, world!");
+use core::panic::PanicInfo;
+
+mod arch;
+
+#[macro_export]
+macro_rules! debug_print {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            use core::fmt::Write;
+            let mut debug_writer = arch::DebugWriter::new();
+            let _ = write!(debug_writer, "[KERNEL] ");
+            let _ = writeln!(debug_writer, $($arg)*);
+        }
+    };
 }
