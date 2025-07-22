3dd80cc679265837eaad4429972f1f4e90fb48cd
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-07-22 22:02:08 +0900

    fix: typo

--- kernel/src/arch/arm/exceptions.rs
+++ kernel/src/arch/arm/exceptions.rs
@@ -248,22 +248,22 @@ impl ExceptionManager {
     }
 
     /// Configure exception handling features
-    fn configure_exception(&self) -> Result<(), &'static str> {
+    fn configure_exceptions(&self) -> Result<(), &'static str> {
         // SAFETY: We're configuring exception handling
         unsafe {
             let mut sctlr: u32;
-
+            
             // Read SCTLR
             asm!("mrc p15, 0, {}, c1, c0, 0", out(reg) sctlr, options(nomem, nostack));
-
+            
             // Ensure low vectors (clear V bit)
             sctlr &= !(1 << 13);
-
+            
             // Write back SCTLR
             asm!("mcr p15, 0, {}, c1, c0, 0", in(reg) sctlr, options(nomem, nostack));
             asm!("isb", options(nomem, nostack));
         }
-
+        
         Ok(())
     }
 
