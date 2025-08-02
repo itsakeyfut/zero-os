9edc0c5deabd49a558b526c99d55a30acabf1f08
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-08-02 11:17:59 +0900

    chore: remove old debug_print

--- kernel/src/macros/mod.rs
+++ kernel/src/macros/mod.rs
@@ -166,17 +166,3 @@ macro_rules! kernel_ensure {
         }
     };
 }
-
-#[macro_export]
-macro_rules! debug_print {
-    ($($arg:tt)*) => {
-        #[cfg(debug_assertions)]
-        {
-            use core::fmt::Write;
-            if let Ok(mut debug_writer) = $crate::arch::DebugWriter::new() {
-                let _ = write!(debug_writer, "[KERNEL] ");
-                let _ = writeln!(debug_writer, $($arg)*);
-            }
-        }
-    };
-}
